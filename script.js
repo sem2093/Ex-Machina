005c9bde4890fb5e5d5f4863d0bf1753439c6680
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-05-22 21:26:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
-function updateDescription() {
-    // Get the selected option value and text
-    var dropdown = document.getElementById("dropdown");
-    var value = dropdown.value;
-    var text = dropdown.options[dropdown.selectedIndex].text;
-    
-    // Update the description element based on the selected option
-    var description = document.getElementById("description");
-    if (value === "option1") {
-      description.innerHTML = "This project is an AI chat bot that uses natural language processing to communicate with users.";
-    } else if (value === "option2") {
-      description.innerHTML = "This project is a coding educational platform that provides a coding dictionary and step by step walk throughs.";
-    } else if (value === "option3") {
-      description.innerHTML = "This project is intended to make digesting the news simpler by fetching a priortized summary of daily or weekly events. Due to the need of an API key to accesss News APIS which is where the news data would be fetched from I cannot include this project on my website.";
-    } else {
-      description.innerHTML = "";
-    }
+function appendChar(char) {
+  document.getElementById('result').value += char;
+}
+
+function deleteLastChar() {
+  var result = document.getElementById('result').value;
+  document.getElementById('result').value = result.slice(0, -1);
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+  var result = document.getElementById('result').value;
+  try {
+    var calculatedResult = eval(result);
+    document.getElementById('result').value = calculatedResult;
+  } catch (error) {
+    document.getElementById('result').value = 'Error';
   }
-
-var input = document.getElementById("input");
-var button = document.getElementById("submit-button");
-var responseBox = document.getElementById("response");
-
-button.addEventListener("click", function() {
-  var message = input.value;
-  input.value = "";
-  // Process the user's message and generate a response
-  var response = processMessage(message);
-  // Display the response to the user
-  displayMessage(response, responseBox);
-});
-
-// Define the chatbot's behavior and responses
-function processMessage(message) {
-  // Add your own logic here to generate a response based on the user's message
-  return "Hello! How can I help you today?";
 }
 
-function displayMessage(message, box) {
-  var messageElement = document.createElement("div");
-  messageElement.innerText = message;
-  box.appendChild(messageElement);
-}
 
