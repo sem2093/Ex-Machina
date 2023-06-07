c6ce228b496ffdf01f2c26a2f06b42d4edb7c0c8
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-07 11:35:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 document.getElementById("h1").innerHTML="Steven Miller";
+document.getElementById("h2am").innerHTML="About me";
+
+document.getElementById("pam").innerHTML="Hi, my name is Steven Miller and Im a developer. Im a self learner and am both curious and driven to improve constantly.";
+
+document.getElementById("pc").innerHTML="Steven Miller 2023 All rights reserved";
+
+
+document.getElementById("pedu").innerHTML="I graduated from Alta highschool in Sandy UT in 2012. After highschool I went on to study at Brigham Young University in Provo Ut.";
+
+document.getElementById("h2edu").innerHTML="Education";
+
+document.getElementById("h2ex").innerHTML="Experience";
+
+document.getElementById("pex").innerHTML="I have a well rounded work experience from being a customer service representative to a Master Fire Alarm Technician.";
+
+document.getElementById("pp").innerHTML="937-474-9664";
+
+document.getElementById("pem").innerHTML="sem2093@gmail.com";
+
+document.getElementById("h3con").innerHTML="Contact";
+
+document.getElementById("h3f").innerHTML="Follow";
+
+document.getElementById("h2edu").innerHTML="Education";
+
+document.getElementById("h2co").innerHTML="Competencies";
+
+document.getElementById("h2ce").innerHTML="Certificates";
