b9222b288865412a72d35bfbd0748a4820ad2944
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-07 06:44:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ document
 document 
 .getElementById("h2am").innerHTML = "About Me";
 document 
-.getElementById("h2e").innerHTML = "Education";
+.getElementById("h2ed").innerHTML = "Education";
 document 
-.getElementById("h2e").innerHTML = "Education";
+.getElementById("ped").innerHTML = "I graduated high school in 2012 from Alta High School in Sandy, UT. After high school, I went to Brigham 
+			Young University, where I first began programming in a computer science course.
+			Although I have not yet obtained a college degree, my passion for learning 
+			and curiosity has helped me continue learning even though I'm not enrolled in school any longer.";
 document 
-.getElementById("h2e").innerHTML = "Education";
+.getElementById("h2ex").innerHTML = "Experience";
 document 
-.getElementById("h2e").innerHTML = "Education";
+.getElementById("h2co").innerHTML = "Competencies";
 document 
-.getElementById("h2e").innerHTML = "Education";
+.getElementById("h2ce").innerHTML = "Certifications";
 document 
 .getElementById("h2e").innerHTML = "Education";
