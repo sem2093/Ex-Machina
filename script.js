7d77d7c59eae45be6aaeae0bb61e9682c1c83970
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-07 06:32:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document 
-.getElementById("h1").innerHTML = "welcome to Javascript Baby";
+.getElementById("h1").innerHTML = "Steven Miller";
 document 
 .getElementById("h2am").innerHTML = "About Me";
+document 
+.getElementById("h2e").innerHTML = "Education";
+document 
+.getElementById("h2e").innerHTML = "Education";
+document 
+.getElementById("h2e").innerHTML = "Education";
+document 
+.getElementById("h2e").innerHTML = "Education";
+document 
+.getElementById("h2e").innerHTML = "Education";
 document 
 .getElementById("h2e").innerHTML = "Education";
