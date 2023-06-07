e4583e972e62c13ebc42d759c72ff2f9af8c064d
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-07 06:30:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document 
 .getElementById("h1").innerHTML = "welcome to Javascript Baby";
 document 
-.getElementById("h1").innerHTML = "welcome to Javascript Baby";
+.getElementById("h2am").innerHTML = "About Me";
 document 
-.getElementById("h1").innerHTML = "welcome to Javascript Baby";
+.getElementById("h2e").innerHTML = "Education";
