917145c8de14194193a8a69a96b271edc98f0924
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-07 20:11:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ document.getElementById("pam").innerHTML="Hi, my name is Steven Miller and Im a 
 
 document.getElementById("pc").innerHTML="Steven Miller 2023 All rights reserved";
 
-
 document.getElementById("pedu").innerHTML="I graduated from Alta highschool in Sandy UT in 2012. After highschool I went on to study at Brigham Young University in Provo Ut.";
-
-document.getElementById("h2edu").innerHTML="Education";
 
 document.getElementById("h2ex").innerHTML="Experience";
 
