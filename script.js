c1bde4e92a4c957e82e63cfe3017d5b3c25568d5
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-07 20:01:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -28,46 +28,34 @@ document.getElementById("h2co").innerHTML="Competencies";
 
 document.getElementById("h2ce").innerHTML="Certificates";
 
-let el = document.querySelector("#htmlco"); 
-el.src= 
-"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png";
+let el;
 
-let el = document.querySelector("#cssco"); 
-el.src= 
-"https://cdn-icons-png.flaticon.com/512/5968/5968242.png";
+el = document.querySelector("#cssco");
+el.src = "https://cdn-icons-png.flaticon.com/512/5968/5968242.png";
 
-let el = document.querySelector("#c#co"); 
-el.src= 
-"https://static-00.iconduck.com/assets.00/c-sharp-c-icon-456x512-9sej0lrz.png";
+el = document.querySelector("#c#co");
+el.src = "https://static-00.iconduck.com/assets.00/c-sharp-c-icon-456x512-9sej0lrz.png";
 
-let el = document.querySelector("#c++co"); 
-el.src= 
-"https://cdn-icons-png.flaticon.com/512/6132/6132222.png";
+el = document.querySelector("#c++co");
+el.src = "https://cdn-icons-png.flaticon.com/512/6132/6132222.png";
 
-let el = document.querySelector("#sqlco"); 
-el.src= 
-"https://static-00.iconduck.com/assets.00/sql-database-generic-icon-380x512-ez505zus.png";
+el = document.querySelector("#sqlco");
+el.src = "https://static-00.iconduck.com/assets.00/sql-database-generic-icon-380x512-ez505zus.png";
 
-let el = document.querySelector("#pythonco"); 
-el.src= 
-"https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png";
+el = document.querySelector("#pythonco");
+el.src = "https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png";
 
-let el = document.querySelector("#jsonco"); 
-el.src= 
-"https://cdn-icons-png.flaticon.com/512/8297/8297373.png";
+el = document.querySelector("#jsonco");
+el.src = "https://cdn-icons-png.flaticon.com/512/8297/8297373.png";
 
-let el = document.querySelector("#jsco"); 
-el.src= 
-"https://cdn-icons-png.flaticon.com/512/5968/5968292.png";
+el = document.querySelector("#jsco");
+el.src = "https://cdn-icons-png.flaticon.com/512/5968/5968292.png";
 
-let el = document.querySelector("#reactco"); 
-el.src= 
-"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png";
+el = document.querySelector("#reactco");
+el.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png";
 
-let el = document.querySelector("#gitco"); 
-el.src= 
-"https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png";
+el = document.querySelector("#gitco");
+el.src = "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png";
 
-let el = document.querySelector("#githubco"); 
-el.src= 
-"https://cdn-icons-png.flaticon.com/512/25/25231.png";
+el = document.querySelector("#githubco");
+el.src = "https://cdn-icons-png.flaticon.com/512/25/25231.png";
