1d2a08237fb45d8ce2111cd9215a40f2366b2176
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-15 03:13:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ document.getElementById("pc").innerHTML="Steven Miller 2023 All rights reserved"
 
 document.getElementById("pedu").innerHTML="I graduated from Alta highschool in Sandy UT in 2012. After highschool I went on to study at Brigham Young University in Provo Ut.";
 
-document.getElementById("h2ex").innerHTML="Experience";
-
 document.getElementById("pex").innerHTML="I have a well rounded work experience from being a customer service representative to a Master Fire Alarm Technician.";
 
 document.getElementById("pp").innerHTML="937-474-9664";
@@ -27,12 +25,6 @@ document.getElementById("pem").innerHTML="sem2093@gmail.com";
 document.getElementById("h3con").innerHTML="Contact";
 
 document.getElementById("h3f").innerHTML="Follow";
-
-document.getElementById("h2edu").innerHTML="Education";
-
-document.getElementById("h2co").innerHTML="Competencies";
-
-document.getElementById("h2ce").innerHTML="Certificates";
 /*
 The following section of code 
 sets source(src) of the image(img)
