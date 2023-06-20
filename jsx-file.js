5b431b4ae0f19d4cf2d80a02c148c90daa3a334c
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 06:21:37 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -1,23 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { jsx as _jsx } from "react/jsx-runtime";
 import { jsxs as _jsxs } from "react/jsx-runtime";
-ReactDOM.render( /*#__PURE__*/_jsxs("div", {
-  "data-aos": "fade-in",
-  id: "am",
-  children: [/*#__PURE__*/_jsx("div", {
-    class: "container",
-    children: /*#__PURE__*/_jsxs("h2", {
-      id: "h2am",
-      children: ["About", /*#__PURE__*/_jsx("span", {
-        class: "auto-typeh2am"
-      })]
-    })
-  }), /*#__PURE__*/_jsx("div", {
-    class: "container",
-    children: /*#__PURE__*/_jsxs("p", {
-      id: "pam",
-      children: ["I'm", /*#__PURE__*/_jsx("span", {
-        class: "auto-typepam"
-      })]
-    })
-  })]
-}), document.getElementById("amroot"));
+const App = () => /*#__PURE__*/_jsx("div", {
+  children: /*#__PURE__*/_jsxs("div", {
+    "data-aos": "fade-in",
+    id: "am",
+    children: [/*#__PURE__*/_jsx("div", {
+      className: "container",
+      children: /*#__PURE__*/_jsxs("h2", {
+        id: "h2am",
+        children: ["About", /*#__PURE__*/_jsx("span", {
+          className: "auto-typeh2am"
+        })]
+      })
+    }), /*#__PURE__*/_jsx("div", {
+      className: "container",
+      children: /*#__PURE__*/_jsxs("p", {
+        id: "pam",
+        children: ["I'm", /*#__PURE__*/_jsx("span", {
+          className: "auto-typepam"
+        })]
+      })
+    })]
+  })
+});
+ReactDOM.render( /*#__PURE__*/_jsx(App, {}), document.getElementById("amroot"));
