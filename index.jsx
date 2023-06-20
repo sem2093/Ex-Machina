7197dbf82c06ca46763b2dce049b3fedc2122bd6
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 09:18:00 -0800

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const header = (
+<h1>
+Hello World
+</h1>
+);
+
 ReactDOM.render(
-<h1>Hello World</h1>,
+header,
 document.getElementById("root")
 );
 
