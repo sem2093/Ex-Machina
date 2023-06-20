a1b995736436fdf6697234e366614de8f2bf08f7
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 05:36:52 -0800

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
+ReactDOM.render(
+  <div data-aos="fade-in" id="am">
+  <div class="container">
+  <h2 id="h2am">About<span class="auto-typeh2am"></span></h2>
+  </div>
+  <div class="container">
+<p id="pam">I'm<span class="auto-typepam"></span></p>
+</div>
+</div>,
+  document.getElementById("amroot")
+);
