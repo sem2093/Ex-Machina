3accdb2f996666df0c68fc6dc3c29acf0f4d523a
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 15:37:30 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -1,11 +1,11 @@
 var input = 'const getMessage = () => "Hello World";';
 var output = Babel.transform(input, { presets: ['es2015'] }).code;
 
-
+//second hello world 
 const getMessage = () => "Hello World";
 document.getElementById('op2').innerHTML = getMessage(); 
 
-
+//react example from jsanywhere
 const App = ({ name }) => {
   return (
     <h1>{name}</h1>
@@ -16,3 +16,10 @@ ReactDOM.render(
   <App name="React Debug" />,
   document.getElementById('app')
 );
+//react example from mimo
+
+ReactDOM.render( 
+<h2> About Me </h2>,
+document.getElementById("root")
+
+  );
