2a99942be862c8297c1db728d57fbc67fec8ace4
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 05:47:17 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -1,1 +1,25 @@
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { jsx as _jsx } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
+ReactDOM.render( /*#__PURE__*/_jsxs("div", {
+  "data-aos": "fade-in",
+  id: "am",
+  children: [/*#__PURE__*/_jsx("div", {
+    class: "container",
+    children: /*#__PURE__*/_jsxs("h2", {
+      id: "h2am",
+      children: ["About", /*#__PURE__*/_jsx("span", {
+        class: "auto-typeh2am"
+      })]
+    })
+  }), /*#__PURE__*/_jsx("div", {
+    class: "container",
+    children: /*#__PURE__*/_jsxs("p", {
+      id: "pam",
+      children: ["I'm", /*#__PURE__*/_jsx("span", {
+        class: "auto-typepam"
+      })]
+    })
+  })]
+}), document.getElementById("amroot"));
