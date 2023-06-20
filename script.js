3f5d64134ea034f4872eee8bc732715c6962ea61
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 07:50:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,6 @@ This is a multi-line comment.
 It can span multiple lines.
 */
 /*
-The following section of code 
-sets the text(innerHTML) of the 
-element. 
-document.getElementById("elementId").innerHTML="text"
-*/
-
-
-document.getElementById("pc").innerHTML=" &copy; Steven Miller 2023 All rights reserved";
-
-document.getElementById("pp").innerHTML="937-474-9664";
-
-document.getElementById("pem").innerHTML="sem2093@gmail.com";
 
 /*
 The following section of code 
@@ -54,89 +42,3 @@ j.src = "https://cdn-icons-png.flaticon.com/512/25/25231.png";
 let k= document.querySelector("#htmlco");
 k.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png";
 
-//The following code is type text animation. 
-var typed = new Typed(".auto-typeh1", {  
-  strings :[" Steven Everett Miller II."," a Developer."],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-var typed = new Typed(".auto-typeh2am", {  
-  strings :["Me"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-    var typed = new Typed(".auto-typeh2edu", {  
-  strings :["Education"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-    var typed = new Typed(".auto-typeh2ex", {  
-  strings :["Experience"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-    var typed = new Typed(".auto-typeh2co", {  
-  strings :["Competencies"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-    var typed = new Typed(".auto-typeh2ce", {  
-  strings :["Certificates"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-    var typed = new Typed(".auto-typeh3con", {  
-  strings :["Me"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-    var typed = new Typed(".auto-typeh3f", {  
-  strings :["Me"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-    var typed = new Typed(".auto-typepam", {  
-  strings :["A self learner.","Determined to constantly improve."],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-     var typed = new Typed(".auto-typepedu", {  
-  strings :["Graduated in 2012 from Alta Highschool in Sandy, UT.","Studied at Brigham Young University in Provo, UT."],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-     var typed = new Typed(".auto-typepex", {  
-  strings :["Master Fire Alarm Technician.","Customer Service Representative.","Seafood Processor"],    
-  typeSpeed :50,
-  backSpeed :25,
-  loop :true
-    });
-  //The following code is the Vanta.net background animation 
-    VANTA.NET({
-  el: "#animation-wrapper",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0xffffff,
-  backgroundColor: 0x0,
-  points: 20.00,
-  spacing: 20.00
-});
-//the following code is for the animate on scroll feature.
-AOS.init({
-    duration:3000
-});
