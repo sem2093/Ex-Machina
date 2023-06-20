7c9136f56c3fba731deb0146cae56e1258be7f78
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 09:35:59 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -1,21 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { jsx as _jsx } from "react/jsx-runtime";
-import { jsxs as _jsxs } from "react/jsx-runtime";
-const App = () => /*#__PURE__*/_jsxs("div", {
-  id: "am",
-  children: [/*#__PURE__*/_jsx("div", {
-    className: "container",
-    children: /*#__PURE__*/_jsx("h2", {
-      id: "h2am",
-      children: "About Me"
-    })
-  }), /*#__PURE__*/_jsx("div", {
-    className: "container",
-    children: /*#__PURE__*/_jsx("p", {
-      id: "pam",
-      children: "I'm a motivated self-leaner eager to master development."
-    })
-  })]
+const header = /*#__PURE__*/_jsx("h1", {
+  children: "Hello World"
 });
-ReactDOM.render( /*#__PURE__*/_jsx(App, {}), document.getElementById("amroot"));
+ReactDOM.render(header, document.getElementById("root"));
