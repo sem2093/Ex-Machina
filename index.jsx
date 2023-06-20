08c4b9d7f6a6840801fc3367682f1a5a20d0c853
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 08:23:04 -0800

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => (
-  <div>
-    <div data-aos="fade-in" id="am">
+    <div id="am">
       <div className="container">
-        <h2 id="h2am">About<span className="auto-typeh2am"></span></h2>
+        <h2 id="h2am">About Me</h2>
       </div>
       <div className="container">
         <p id="pam">I'm<span className="auto-typepam"></span></p>
       </div>
     </div>
-  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById("amroot"));
