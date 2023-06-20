cb1313daaf411054a9f7bfa725650e82f51ceb1a
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 14:11:31 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -6,6 +6,3 @@ const getMessage = () => "Hello World";
 document.getElementById('op2').innerHTML = getMessage(); 
 
 
-const getMessage = () => "Hello World";
-document.getElementById('app').innerHTML = getMessage(); 
-
