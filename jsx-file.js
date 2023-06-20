335bd25964a2c3076889c59655e2fe45360a4992
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 13:14:46 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -1,2 +1,2 @@
 var input = 'const getMessage = () => "Hello World";';
-34	var output = Babel.transform(input, { presets: ['es2015'] }).code;
+var output = Babel.transform(input, { presets: ['es2015'] }).code;
