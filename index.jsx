25206ee3158c80a440cdc043ce0d504168cf716c
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 06:17:16 -0800

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,11 +1,17 @@
-ReactDOM.render(
-  <div data-aos="fade-in" id="am">
-  <div class="container">
-  <h2 id="h2am">About<span class="auto-typeh2am"></span></h2>
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <div>
+    <div data-aos="fade-in" id="am">
+      <div className="container">
+        <h2 id="h2am">About<span className="auto-typeh2am"></span></h2>
+      </div>
+      <div className="container">
+        <p id="pam">I'm<span className="auto-typepam"></span></p>
+      </div>
+    </div>
   </div>
-  <div class="container">
-<p id="pam">I'm<span class="auto-typepam"></span></p>
-</div>
-</div>,
-  document.getElementById("amroot")
 );
+
+ReactDOM.render(<App />, document.getElementById("amroot"));
