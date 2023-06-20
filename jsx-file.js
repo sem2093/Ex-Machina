6a3581b5024b2dd16c34313475904d47b8ea85e9
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-20 08:28:52 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -2,28 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { jsx as _jsx } from "react/jsx-runtime";
 import { jsxs as _jsxs } from "react/jsx-runtime";
-const App = () => /*#__PURE__*/_jsx("div", {
-  children: /*#__PURE__*/_jsxs("div", {
-    "data-aos": "fade-in",
-    id: "am",
-    children: [/*#__PURE__*/_jsx("div", {
-      className: "container",
-      children: /*#__PURE__*/_jsxs("h2", {
-        id: "h2am",
-        children: ["About", /*#__PURE__*/_jsx("span", {
-          className: "auto-typeh2am"
-        })]
-      })
-    }), /*#__PURE__*/_jsx("div", {
-      className: "container",
-      children: /*#__PURE__*/_jsxs("p", {
-        id: "pam",
-        children: ["I'm", /*#__PURE__*/_jsx("span", {
-          className: "auto-typepam"
-        })]
-      })
-    })]
-  })
+const App = () => /*#__PURE__*/_jsxs("div", {
+  id: "am",
+  children: [/*#__PURE__*/_jsx("div", {
+    className: "container",
+    children: /*#__PURE__*/_jsx("h2", {
+      id: "h2am",
+      children: "About Me"
+    })
+  }), /*#__PURE__*/_jsx("div", {
+    className: "container",
+    children: /*#__PURE__*/_jsx("p", {
+      id: "pam",
+      children: "I'm a motivated self-leaner eager to master development."
+    })
+  })]
 });
 ReactDOM.render( /*#__PURE__*/_jsx(App, {}), document.getElementById("amroot"));
-
