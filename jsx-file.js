e066df9ab00daf60b3892010e7f9464befb463a4
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-29 21:07:18 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -38,10 +38,6 @@ ReactDOM.render(
     <div>
       <h2>Certificates</h2>
       </div>
-      <iframe src="JavaScriptcertificate.pdf" width="100%" height="600px"></iframe>
-      <iframe src="HTML certification .pdf" width="100%" height="600px"></iframe>
-      <iframe src="SQLcertificate.pdf" width="100%" height="600px"></iframe>
-  
  </div>
 
 document.getElementById("main")
