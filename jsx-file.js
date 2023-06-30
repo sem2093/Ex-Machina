6787051ec0af769f79839ef39642cb211c1003ad
sem2093 <131418264+sem2093@users.noreply.github.com>
2023-06-30 03:50:57 -0800

    Update jsx-file.js

--- jsx-file.js
+++ jsx-file.js
@@ -13,7 +13,7 @@ ReactDOM.render(
    <h1>Hi I'm Steven Miller & I'm a Developer.</h1>
   </div>
   <div>
-<img src={require('C64EFC3C-ACBA-4CB6-BC6F-4D4D34F6236C.png')} alt={"logo"}/>
+<img src={require('./C64EFC3C-ACBA-4CB6-BC6F-4D4D34F6236C.png')} alt={"logo"}/>
 </div>
 </div>,
   document.getElementById("header")
