e76a1a85a5f8728275e15d9a9426e43bc66c0704
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-24 05:47:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
+const carousel = new bootstrap.Carousel('#myCarousel')
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Follow me on github and I'll follow you back!', 'success')
-  })
-}
-function showAlert() {
-    alert('Follow me on github and I'll follow you back!');
-}
