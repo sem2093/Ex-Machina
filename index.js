7e159371aab219cec983e6a262350fec1186016c
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-09-14 03:58:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ var htmlTag = document.documentElement;
 // Add an attribute and value to the <html> tag
 htmlTag.setAttribute("data-bs-theme", "dark");
 }
+
+
+function lightMode(){
+
+// Get a reference to the <html> tag
+var htmlTag = document.documentElement;
+
+// Add an attribute and value to the <html> tag
+htmlTag.setAttribute("data-bs-theme", "light");
+}
