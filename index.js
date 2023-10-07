0b16146ecc67fe3183a325340cf65e0b34739e10
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-07 13:36:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ var htmlTag = document.documentElement;
 // Add an attribute and value to the <html> tag
 htmlTag.setAttribute("data-bs-theme", "light");
 }
+
+function blueMode(){
+  
+
+var bluediv = document.getElementById("myElement");
+div.classList.remove("active");
+var element = document.getElementById("myElement");
+div.classList.add("p-3,text-primary-emphasis, bg-primary-subtle, border, border-primary-subtle, rounded-3");
+}
