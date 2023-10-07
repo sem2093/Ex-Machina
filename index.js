dc2861bfb53e4ff42549a24c320640f9f58b229d
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-07 15:58:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ htmlTag.setAttribute("data-bs-theme", "light");
 function blueMode(){
   
 var bluediv = document.getElementById("navtest1");
-bluediv.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluediv.classList.remove("bg-body-tertiary", "rounded");
 bluediv.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
 
 var bluenav = document.getElementById("navtest2");
-bluenav.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluenav.classList.remove("bg-body-tertiary", "rounded");
 bluenav.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
 
 var bluedivs = document.getElementById("navtest3");
-bluedivs.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluedivs.classList.remove("bg-body-tertiary", "rounded");
 bluedivs.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
   
 var bluenavs = document.getElementById("navtest4");
-bluenavs.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluenavs.classList.remove("bg-body-tertiary", "rounded");
 bluenavs.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
 
 }
