5b45d0e1d2ac2e17d42b6cd0ae1f194f7ec27839
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-07 15:41:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,20 @@ htmlTag.setAttribute("data-bs-theme", "light");
 
 function blueMode(){
   
-
-var bluediv = document.getElementById("test");
-bluediv.classList.remove("fluid-container");
+var bluediv = document.getElementById("navtest1");
+bluediv.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
 bluediv.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+
+var bluenav = document.getElementById("navtest2");
+bluenav.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
+bluenav.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+
+var bluedivs = document.getElementById("navtest3");
+bluedivs.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
+bluedivs.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+  
+var bluenavs = document.getElementById("navtest4");
+bluenavs.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
+bluenavs.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+
 }
