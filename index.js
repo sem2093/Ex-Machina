c1fda1744fcf1f37905784b52fedcb982e288374
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-07 15:55:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ htmlTag.setAttribute("data-bs-theme", "light");
 function blueMode(){
   
 var bluediv = document.getElementById("navtest1");
-bluediv.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
-bluediv.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+bluediv.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluediv.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
 
 var bluenav = document.getElementById("navtest2");
-bluenav.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
-bluenav.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+bluenav.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluenav.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
 
 var bluedivs = document.getElementById("navtest3");
-bluedivs.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
-bluedivs.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+bluedivs.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluedivs.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
   
 var bluenavs = document.getElementById("navtest4");
-bluenavs.classList.remove("shadow-lg","p-3","mb-5","bg-body-tertiary", "rounded" ,"border");
-bluenavs.classList.add("p-3","text-primary-emphasis", "bg-primary-subtle", "border", "border-primary-subtle", "rounded-3");
+bluenavs.classList.remove("mb-5","bg-body-tertiary", "rounded");
+bluenavs.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
 
 }
