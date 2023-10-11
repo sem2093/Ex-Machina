3c2f0cbb3a2bdef8b168bfb209e330c48a0f6aef
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-10 19:01:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,22 +17,5 @@ var htmlTag = document.documentElement;
 htmlTag.setAttribute("data-bs-theme", "light");
 }
 
-function blueMode(){
-  
-var bluediv = document.getElementById("navtest1");
-bluediv.classList.remove("bg-body-tertiary", "rounded");
-bluediv.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
 
-var bluenav = document.getElementById("navtest2");
-bluenav.classList.remove("bg-body-tertiary", "rounded");
-bluenav.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
-
-var bluedivs = document.getElementById("navtest3");
-bluedivs.classList.remove("bg-body-tertiary", "rounded");
-bluedivs.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
-  
-var bluenavs = document.getElementById("navtest4");
-bluenavs.classList.remove("bg-body-tertiary", "rounded");
-bluenavs.classList.add("text-primary-emphasis", "bg-primary-subtle", "border-primary-subtle", "rounded-3");
-
-}
+console.log("Hello world");
