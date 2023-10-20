5239751e4460d51b10d229e9452d4afdcff5ae3e
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-19 23:14:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,39 @@ htmlTag.setAttribute("data-bs-theme", "light");
 }
 
 
-console.log("Hello world");
+function decideResponse(){
+let input = document.getElementById("userInput");
+
+let inputStr= input.value;
+
+
+const chat = document.getElementById("chat");
+
+let history = chat.innerHTML;
+
+let inputValue= inputStr.toLowerCase();
+
+
+const tokens = inputValue.split(/\s+/);
+
+let response="";
+
+for (let i=0;i<tokens.length;i++) { if (!isNaN(parseInt(tokens[i]))){ input.setAttribute("type","number"); let calculated = eval(inputStr);
+response = "="+calculated; 
+input.setAttribute("type","text");
+}
+
+else if (tokens[i]==="hello"||tokens[i]==="hi"||tokens[i]==="hey"||tokens[i]==="yo"
+){  response = "hello";
+      break; }
+
+else if (tokens[i]==="date"||tokens[i]==="time") { response = new Date(); 
+break;}
+
+else { response = "okay"}
+
+};
+
+chat.innerHTML = "<br>Me:<br>"+inputStr + "  <br>Bot: <br>" + response + history; 
+
+}
